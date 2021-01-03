2ab4fda787767f133d09acc44896420b0adc9e2d
Nickpan0925 <a0920215979@gmail.com>
2021-01-04 00:01:09 +0800

    update Form.vue

--- src/views/back/Form.vue
+++ src/views/back/Form.vue
@@ -102,6 +102,14 @@ export default {
 .order-wrapper{
     width: 100%;
     padding: 20px 20px 0 250px;
+    background-color: #C8EBDF;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -999;
+    overflow:scroll;
 }
 .order-container{
      max-width: 1200px;
